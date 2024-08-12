4c1fe1f46a06ab7c6d14dd44d230b1b032ab06cd
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-08-12 23:40:43 +0530

    solve firebase setup

---
+++ config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  getAuth,
+} from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {collection, getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+export const db = getFirestore(app);
+export const tripRef = collection(db, 'trips');
+export const expensesRef = collection(db, 'expenses');
+
+export default app;
